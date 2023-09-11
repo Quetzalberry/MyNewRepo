4bd1ebeea4837f32b9e84d1a333db952723b728c
Quetzalberry <denverheneghan@gmail.com>
2023-09-10 19:23:19 -0500

    Challenge1

---
+++ Challenge1/Assets/Challenge 1/Scripts/AddScoreOnce.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class AddScoreOnce : MonoBehaviour
+{
+    private bool triggered = false;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            ManageScore.score++;
+        }
+    }
+}
---
+++ Challenge1/Assets/Challenge 1/Scripts/LoseConditions.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LoseConditions : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (transform.position.y < -51)
+        {
+            ManageScore.gameOver = true;
+        }
+
+        if(transform.position.y > 80)
+        {
+            ManageScore.gameOver = true;
+        }
+    }
+}
