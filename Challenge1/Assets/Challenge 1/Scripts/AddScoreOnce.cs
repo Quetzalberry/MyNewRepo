3ec40d8a1c69020bac50bdd70ec82a8a27772c92
Quetzalberry <denverheneghan@gmail.com>
2023-09-10 19:51:12 -0500

    Challenge1Runthrough Finished

--- Challenge1/Assets/Challenge 1/Scripts/AddScoreOnce.cs
+++ Challenge1/Assets/Challenge 1/Scripts/AddScoreOnce.cs
@@ -2,7 +2,7 @@
  * Denver Heneghan
  * Assignment 2, Challenge 1
  * AddScoreOnce
- * This script increases the score by 1 every time the player touches a trigger zone. Once the score reaches 5, triggered is
+ * This script increases the score by 1 every time the player touches a trigger zone. Once the score has reached 5, triggered is
  * confirmed to be true, and this lets ManageScore print out "You Win!"
  */
 
