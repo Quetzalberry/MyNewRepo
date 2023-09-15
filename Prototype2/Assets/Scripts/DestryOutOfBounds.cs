3a49b957245f22f14a712295681004755bc80d1a
Quetzalberry <denverheneghan@gmail.com>
2023-09-15 13:43:15 -0500

    Prototype2 destroy objects

---
+++ Prototype2/Assets/Scripts/DestryOutOfBounds.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DestryOutOfBounds : MonoBehaviour
+{
+    public float topBound = 20;
+    public float bottomBound = -10;
+
+    // Update is called once per frame
+    void Update()
+    {
+        //If food goes out of bounds.
+        if (transform.position.z > topBound)
+        {
+            Destroy(gameObject);
+        }
+
+        //If animals go out of bounds.
+        if (transform.position.z < bottomBound)
+        {
+            Debug.Log("Game Over!");
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Prototype2/Assets/Scripts/DetectCollisions.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//Attach to food.
+
+public class DetectCollisions : MonoBehaviour
+{
+    private void OnTriggerEnter(Collider other)
+    {
+        Destroy(other.gameObject);
+        Destroy(gameObject);
+    }
+}
