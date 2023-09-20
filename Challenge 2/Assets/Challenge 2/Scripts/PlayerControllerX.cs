c5bd1dd78ffce01e487e5d683086fa28f59d8613
Quetzalberry <denverheneghan@gmail.com>
2023-09-19 20:08:50 -0500

    Challenge 2 Finished

--- Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
+++ Challenge 2/Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -3,7 +3,7 @@
 * PlayerControllerX
 * Challenge 2
 * For this script, I added a timer. I set the timer for half a second. Once a dog is spawned, the timer needs to reach zero before it can
-* be spawned again. This way the player can not rapidly spawn dogs. The player needs to wait 0.5 seconds to spawn the next dog.
+* be spawned again. This way the player can not rapidly spawn dogs. The player needs to wait 1.0 seconds to spawn the next dog.
 */
 
 using System.Collections;
@@ -13,22 +13,27 @@ using UnityEngine;
 public class PlayerControllerX : MonoBehaviour
 {
     public GameObject dogPrefab;
-    public float time = 0.5f;
-    public float timer = Time.time;
+    private float time = 1.0f;
+    private bool countdown = false;
 
     // Update is called once per frame
     void Update()
     {
-        timer += Time.deltaTime;
-
-        if (timer >= time)
+        if (Input.GetKeyDown(KeyCode.Space))
         {
-            if (Input.GetKeyDown(KeyCode.Space))
+            if (countdown == false)
             {
                 Dog();
-                timer = 0;
+                StartCoroutine(Time());
             }
         }
+    }
+
+    IEnumerator Time()
+    {
+        countdown = true;
+        yield return new WaitForSeconds(time);
+        countdown = false;
     }
 
     void Dog()
