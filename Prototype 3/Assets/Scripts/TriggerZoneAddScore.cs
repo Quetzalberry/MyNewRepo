08f7dcdbd0711715eb01b52376107d9285051702
Quetzalberry <denverheneghan@gmail.com>
2023-09-23 16:42:15 -0500

    Prototype 3

--- Prototype 3/Assets/Scripts/TriggerZoneAddScore.cs
+++ Prototype 3/Assets/Scripts/TriggerZoneAddScore.cs
@@ -4,15 +4,22 @@ using UnityEngine;
 
 public class TriggerZoneAddScore : MonoBehaviour
 {
+    private UIManager uIManager;
+
+    private bool triggered = false;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        uIManager = GameObject.FindObjectOfType<UIManager>();
     }
 
-    // Update is called once per frame
-    void Update()
+    private void OnTriggerEnter(Collider other)
     {
-        
+        if (other.CompareTag("Player") && !triggered)
+        {
+            triggered = true;
+            uIManager.score++;
+        }
     }
 }
