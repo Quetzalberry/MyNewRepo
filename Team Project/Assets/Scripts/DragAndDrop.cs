7c582c832944b237c8ecc3b956af2d8b7275a3dc
Quetzalberry <denverheneghan@gmail.com>
2023-09-23 22:07:36 -0500

    Sprites

---
+++ Team Project/Assets/Scripts/DragAndDrop.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DragAndDrop : MonoBehaviour
+{
+    private Vector3 click;
+    private Vector3 move;
+
+    private void OnMouseDown()
+    {
+        move = gameObject.transform.position - Camera.main.ScreenToWorldPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, click.z));
+    }
+
+    private void OnMouseDrag()
+    {
+        Vector3 pointOnScreen = new Vector3(Input.mousePosition.x, Input.mousePosition.y, click.z);
+        Vector3 mousePosition = Camera.main.ScreenToWorldPoint(pointOnScreen) + move;
+        transform.position = mousePosition;
+    }
+}
