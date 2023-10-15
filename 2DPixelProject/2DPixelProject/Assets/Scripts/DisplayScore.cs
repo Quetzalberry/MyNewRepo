67ce022fb4ddd2b183fcb36a79388254387619d0
Quetzalberry <denverheneghan@gmail.com>
2023-10-15 14:48:46 -0500

    Completed 2D project

--- 2DPixelProject/2DPixelProject/Assets/Scripts/DisplayScore.cs
+++ 2DPixelProject/2DPixelProject/Assets/Scripts/DisplayScore.cs
@@ -1,3 +1,13 @@
+/*
+* Denver Heneghan
+* DisplayScore
+* 2DPixelProject
+* This script displays the score on screen. It sets points to 0, then it sets the score text to equal "Score: " + points. This means that the score will show
+* whatever number points is. There is a void called increasePoints, and it adds 1 to points when called. Then it prints out the new score text with the new points.
+* Then the script detects if the object this script is attached to has collided with an object tagged Player. If it has, it then calls the increaasePoints
+* void and executes it. This script is attached to the gem objects.
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
--- 2DPixelProject/2DPixelProject/Assets/Scripts/WinScript.cs
+++ 2DPixelProject/2DPixelProject/Assets/Scripts/WinScript.cs
@@ -1,3 +1,11 @@
+/*
+* Denver Heneghan
+* WinScript
+* 2DPixelProject
+* This script is attached to two tiles that are set as triggers. When these tile triggers come into contact with the object tagged as player, it activates the
+* YouWin text to true. The text is set to hidden at the start of the game, so when it is set to true, the text will show up on screen.
+*/
+
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
