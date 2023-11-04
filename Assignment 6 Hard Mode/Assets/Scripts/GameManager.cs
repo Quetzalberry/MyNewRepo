982cb1ff38d93fbeadcdc9fc31da6257d708e10e
Quetzalberry <denverheneghan@gmail.com>
2023-11-04 14:48:30 -0500

    Levels

--- Assignment 6 Hard Mode/Assets/Scripts/GameManager.cs
+++ Assignment 6 Hard Mode/Assets/Scripts/GameManager.cs
@@ -1,10 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
     public int score;
+
+    //A variable to keep track of what level we are on.
+    private string CurrentLevelName = string.Empty;
 
     #region This code makes this class a singleton.
     public static GameManager instance;
@@ -24,4 +28,29 @@ public class GameManager : MonoBehaviour
         }
     }
     #endregion
+
+    //Methods to load and unload scenes.
+    public void LoadLevel(string levelName)
+    {
+        AsyncOperation ao = SceneManager.LoadSceneAsync(levelName, LoadSceneMode.Additive);
+
+        if (ao == null)
+        {
+            Debug.LogError("[GameManager] unable to load level " + levelName);
+            return;
+        }
+
+        CurrentLevelName = levelName;
+    }
+
+    public void UnloadLevel(string levelName)
+    {
+        AsyncOperation ao = SceneManager.UnloadSceneAsync(levelName);
+
+        if (ao == null)
+        {
+            Debug.LogError("[GameManager] unable to unload level " + levelName);
+            return;
+        }
+    }
 }
