1cbc97504f76f9558e24913b212d35fb955c189c
Quetzalberry <denverheneghan@gmail.com>
2023-11-07 18:14:09 -0600

    Easy Mode

---
+++ Assignment 6 Easy/Assets/Assignment6/Scripts/Golem.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Golem : Enemy
+{
+    protected int damage;
+
+    protected override void Awake()
+    {
+        base.Awake();
+        health = 120;
+        GameManager.Instance.score += 2;
+    }
+
+    protected override void Attack(int amount)
+    {
+        Debug.Log("Golem Attacks.");
+    }
+
+    public override void TakeDamage(int amount)
+    {
+        Debug.Log("You took " + amount + " points of damage.");
+    }
+}
