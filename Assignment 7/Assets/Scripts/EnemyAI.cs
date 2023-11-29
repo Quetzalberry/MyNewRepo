3bd7e2a0965b6295c6dd8ee94876ce2a6ed804dd
Quetzalberry <143647124+Quetzalberry@users.noreply.github.com>
2023-11-29 16:03:39 -0600

    Powerup

---
+++ Assignment 7/Assets/Scripts/EnemyAI.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyAI : MonoBehaviour
+{
+    private Rigidbody enemyrb;
+    public GameObject player;
+    public float speed = 3.0f;
+
+    void Start()
+    {
+        enemyrb = GetComponent<Rigidbody>();
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    private void FixedUpdate()
+    {
+        //Add force towards the direction from the player to the enemy.
+
+        //Vector for direction from enemy to player.
+
+        Vector3 lookDirection = (player.transform.position - transform.position).normalized;
+
+        //Add force toward player.
+        enemyrb.AddForce(lookDirection * speed);
+    }
+}
